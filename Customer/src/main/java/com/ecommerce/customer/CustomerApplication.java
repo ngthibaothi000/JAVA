65a95d0a9208f0f09778655230eea42366578242
PhamNguyenHungRG <163432651+PhamNguyenHungRG@users.noreply.github.com>
2024-07-09 13:09:32 +0700

    View

--- Customer/src/main/java/com/ecommerce/customer/CustomerApplication.java
+++ Customer/src/main/java/com/ecommerce/customer/CustomerApplication.java
@@ -9,9 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(value = "com.ecommerce.library.repository")
 @EntityScan(value = "com.ecommerce.library.model")
 public class CustomerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApplication.class, args);
 	}
-
 }
